805047b0da4c5728e23b662db6cdd5ee4274eac9
LuisFer-01 <84158294+LuisFer-01@users.noreply.github.com>
2024-07-07 23:28:18 -0400

    Rayos paso

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Documento</title>
 </head>
 <body>
-    <h1 style="text-align:center">Opps Something Went Wrong <br>
-         Or You Don't Have Permission to Access This Page 
-    <a href="{{ url('/') }}">Go To Home page</a>
+    <h1 style="text-align:center">Opps Algo Salio Mal<br>
+         O Tu no Tienes Permiso para Acceder a esta Pagina 
+    <a href="{{ url('/') }}">Ir a la Pagina de Inicio</a>
     </h1>
 </body>
 </html>
